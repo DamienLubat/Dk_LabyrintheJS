8198a28a243da92ad4befe30e622cea72f5c6d0e
Damien Lubat <damienlubat@outlook.fr>
2023-03-08 23:58:24 +0100

    merge dev master

--- Etape2.js
+++ Etape2.js
@@ -1,190 +1,252 @@
-//direction possible du perso
-var murAll;
+var start = true;
 
-//capteur des evenements
-document.addEventListener('keydown', function (event) {
-    var e = window.event;
-
-    //text
-    console.log(event.code);
-    //int
-    console.log(e.keyCode);
-    console.log(e.keyCode);
-
-    //action de l'utilisateur
-    if (e.keyCode == '38') {
-        // up arrow
-        let posDKtop = parseInt(document.getElementById('dk').style.marginTop.replace('px', ''), 10);
-        let posDKleft = parseInt(document.getElementById('dk').style.marginLeft.replace('px', ''), 10);
-        let marginTopFond = parseInt(document.getElementById('fond').style.marginTop.replace('px', ''), 10);
-        let clientHeightFond = parseInt(document.getElementById('fond').clientHeight, 10);
-
-        var direction = true;
-        var index;
-
-        //check mur
-        if (posDKtop > marginTopFond && clientHeightFond + marginTopFond > posDKtop) {
-            for (let i = 0; i <= 42; i++) {
-
-                if (document.getElementById('mur' + i + '').style.marginLeft == posDKleft + 'px' && document.getElementById('mur' + i + '').style.marginTop == posDKtop - 50 + 'px') {
-                    index = i;
-                    direction = false;
-
-                    break;
-                }
-            }
-
-            if (direction == true) {
-
-                posDKtop = posDKtop - 50;
-
-                //afficher
-                document.getElementById('dk').src = "./img/dk_haut.png"
-                document.getElementById('dk').style.marginTop = posDKtop + "px";
-                document.getElementById('dk').style.marginLeft = posDKleft + "px";
-
-                //hidden
-                //document.getElementById('dk').style.marginTop = posDKtop + "px";
-            }
-        }
+(async function(){
+    while(true){
+        let depArrow = await deplacementArrow();
     }
-    else if (e.keyCode == '40') {
-        // down arrow
-        let posDKtop = parseInt(document.getElementById('dk').style.marginTop.replace('px', ''), 10);
-        let posDKleft = parseInt(document.getElementById('dk').style.marginLeft.replace('px', ''), 10);
-        let widthDK = parseInt(document.getElementById('dk').style.width.replace('px', ''), 10);
-        let marginTopFond = parseInt(document.getElementById('fond').style.marginTop.replace('px', ''), 10);
-        let clientHeightFond = parseInt(document.getElementById('fond').clientHeight, 10);
+})();
 
-        var direction = true;
-        var index;
+run()
+async function run() {
+    //direction possible du perso
+    var murAll;
+    
+    //capteur des evenements
+    document.addEventListener('keydown', async function (event) {
+        var e = window.event;
+        //document.getElementById('0').
+        //text
+        console.log(event.code);
+        //int
+        console.log(e.keyCode);
+        console.log(e.keyCode);
+        
+        
+        
+        //action de l'utilisateur
+        if (e.keyCode == '38') {
+            // up arrow(clavier)
+            let posDKtop = parseInt(document.getElementById('dk').style.marginTop.replace('px', ''), 10);
+            let posDKleft = parseInt(document.getElementById('dk').style.marginLeft.replace('px', ''), 10);
+            let marginTopFond = parseInt(document.getElementById('fond').style.marginTop.replace('px', ''), 10);
+            let clientHeightFond = parseInt(document.getElementById('fond').clientHeight, 10);
+            var posARROWtop = parseInt(250);
+            var posARROWleft = parseInt(document.getElementById('arrow').style.marginLeft.replace('px', ''), 10);
 
-        //check mur
-        if (posDKtop >= marginTopFond && clientHeightFond + marginTopFond - widthDK > posDKtop) {
-            for (let i = 0; i <= 42; i++) {
+            var direction = true;
+            var index;
 
-                if (document.getElementById('mur' + i + '').style.marginLeft == posDKleft + 'px' && document.getElementById('mur' + i + '').style.marginTop == posDKtop + 50 + 'px') {
-                    index = i;
-                    direction = false;
-
-                    break;
-                }
-            }
-
-            if (direction == true) {
-
-                posDKtop = posDKtop + 50;
-
-                //afficher
-                document.getElementById('dk').src = "./img/dk_bas.png"
-                document.getElementById('dk').style.marginTop = posDKtop + "px";
-                document.getElementById('dk').style.marginLeft = posDKleft + "px";
-
-                //hidden
-                //document.getElementById('dk').style.marginTop = posDKtop + "px";
-            }
-        }
-    }
-    else if (e.keyCode == '37') {
-        // left arrow
-        let posDKtop = parseInt(document.getElementById('dk').style.marginTop.replace('px', ''), 10);
-        let posDKleft = parseInt(document.getElementById('dk').style.marginLeft.replace('px', ''), 10);
-        let marginLeftFond = parseInt(document.getElementById('fond').style.marginLeft.replace('px', ''), 10);
-        let clientWidthFond = parseInt(document.getElementById('fond').clientWidth, 10);
-
-        var direction = true;
-        var index;
-
-        //check mur
-        if (posDKleft > marginLeftFond && clientWidthFond + marginLeftFond >= posDKleft) {
-            for (let i = 0; i <= 42; i++) {
-
-                if (document.getElementById('mur' + i + '').style.marginLeft == posDKleft - 50 + 'px' && document.getElementById('mur' + i + '').style.marginTop == posDKtop + 'px') {
-                    index = i;
-                    direction = false
-
-                    break;
-                }
-            }
-            if (direction == true) {
-                //deplacement
-                posDKleft = posDKleft - 50;
-
-                //afficher
-                document.getElementById('dk').src = "./img/dk_gauche.png"
-                document.getElementById('dk').style.marginLeft = posDKleft + "px";
-                document.getElementById('dk').style.marginTop = posDKtop + "px";
-
-
-                //hidden
-                //document.getElementById('dk').style.marginLeft = posDKleft + "px";
-            }
-        }
-    }
-
-    else if (e.keyCode == '39') {
-        // right arrow
-        let posDKtop = parseInt(document.getElementById('dk').style.marginTop.replace('px', ''), 10);
-        let posDKleft = parseInt(document.getElementById('dk').style.marginLeft.replace('px', ''), 10);
-        let widthDK = parseInt(document.getElementById('dk').style.width.replace('px', ''), 10);
-        let marginLeftFond = parseInt(document.getElementById('fond').style.marginLeft.replace('px', ''), 10);
-        let clientWidthFond = parseInt(document.getElementById('fond').clientWidth, 10);
-
-        var direction = true;
-        var index;
-
-        //check mur
-        if (posDKleft >= marginLeftFond && clientWidthFond + marginLeftFond - widthDK > posDKleft) {
-            for (let i = 0; i <= 42; i++) {
-
-
-                if (document.getElementById('mur' + i + '').style.marginLeft == posDKleft + 50 + 'px' && document.getElementById('mur' + i + '').style.marginTop == posDKtop + 'px') {
-                    index = i;
-                    direction = false
-
-                    break;
-                }
-            }
-
-            if (direction == true) {
-                //deplacement
-                posDKleft = posDKleft + 50;
-
-                //afficher
-                document.getElementById('dk').src = "./img/dk_droite.png"
-                document.getElementById('dk').style.marginLeft = posDKleft + "px";
-                document.getElementById('dk').style.marginTop = posDKtop + "px";
-
-                //hidden
-                //document.getElementById('dk').style.marginLeft = posDKleft + "px";
-
-                //Animation fin
-                if (document.getElementById('fin').style.marginLeft == posDKleft + 'px' && document.getElementById('fin').style.marginTop == posDKtop + 'px') {
-                    let fin;
-                    let sonFinDK = sonFin.innerHTML;
+            
+            if (posDKtop > marginTopFond && clientHeightFond + marginTopFond > posDKtop) {
+                for (let i = 0; i <= 42; i++) {
                     
-                    document.getElementById('finAffichage').style.display = "";
-                    document.getElementById('finAffichage').style.zIndex = 4;
-                    document.getElementById('finMessage').style.display = "";
-                    fin = document.getElementsByClassName('stage1')[0];
-                    //fin.remove();
+                    if (document.getElementById('mur' + i + '').style.marginLeft == posDKleft + 'px' && document.getElementById('mur' + i + '').style.marginTop == posDKtop - 50 + 'px') {
+                        index = i;
+                        direction = false;
+                        
+                        //querySelectorAll
+                        
+                        break;
+                    }
+                }
+                
+                if (direction == true) {
+                    
+                    posDKtop = posDKtop - 50;
+                    
+                    //afficher
+                    document.getElementById('dk').src = "./img/dk_haut.png"
+                    document.getElementById('dk').style.marginTop = posDKtop + "px";
+                    document.getElementById('dk').style.marginLeft = posDKleft + "px";
+                    
+                    Dead(posDKleft, posARROWleft, posDKtop, posARROWtop)
 
-
-                    sonFinDK = `<audio src="./img/Final.wav" id="sonFin" style="display:none" controls autoplay ></audio>`;
-                    sonFin.innerHTML = sonFinDK;
-                    document.getElementById('sonFin').play();
-                    setTimeout(document.getElementById('sonFin').pause(), 4000);
-
+                    //hidden
+                    //document.getElementById('dk').style.marginTop = posDKtop + "px";
                 }
             }
         }
-    }
+        else if (e.keyCode == '40') {
+            // down arrow(clavier)
+            let posDKtop = parseInt(document.getElementById('dk').style.marginTop.replace('px', ''), 10);
+            let posDKleft = parseInt(document.getElementById('dk').style.marginLeft.replace('px', ''), 10);
+            let widthDK = parseInt(document.getElementById('dk').style.width.replace('px', ''), 10);
+            let marginTopFond = parseInt(document.getElementById('fond').style.marginTop.replace('px', ''), 10);
+            let clientHeightFond = parseInt(document.getElementById('fond').clientHeight, 10);
+            var posARROWtop = parseInt(250);
+            var posARROWleft = parseInt(document.getElementById('arrow').style.marginLeft.replace('px', ''), 10);
 
-    else if (e.keyCode == '112') {
-        document.getElementById('fondStart').style.zIndex = 0;
-    }
+            var direction = true;
+            var index;
 
-    else if (e.keyCode == '13') {
-        location.href = "index.html"
+            if (posDKtop >= marginTopFond && clientHeightFond + marginTopFond - widthDK > posDKtop) {
+                for (let i = 0; i <= 42; i++) {
+
+                    if (document.getElementById('mur' + i + '').style.marginLeft == posDKleft + 'px' && document.getElementById('mur' + i + '').style.marginTop == posDKtop + 50 + 'px') {
+                        index = i;
+                        direction = false;
+
+                        //querySelectorAll
+
+                        break;
+                    }
+                }
+
+                if (direction == true) {
+
+                    posDKtop = posDKtop + 50;
+
+                    //afficher
+                    document.getElementById('dk').src = "./img/dk_bas.png"
+                    document.getElementById('dk').style.marginTop = posDKtop + "px";
+                    document.getElementById('dk').style.marginLeft = posDKleft + "px";
+                    Dead(posDKleft, posARROWleft, posDKtop, posARROWtop)
+                    //hidden
+                    //document.getElementById('dk').style.marginTop = posDKtop + "px";
+                }
+            }
+        }
+        else if (e.keyCode == '37') {
+
+            // left arrow(clavier)
+            let posDKtop = parseInt(document.getElementById('dk').style.marginTop.replace('px', ''), 10);
+            let posDKleft = parseInt(document.getElementById('dk').style.marginLeft.replace('px', ''), 10);
+            let marginLeftFond = parseInt(document.getElementById('fond').style.marginLeft.replace('px', ''), 10);
+            let clientWidthFond = parseInt(document.getElementById('fond').clientWidth, 10);
+            var posARROWtop = parseInt(250);
+            var posARROWleft = parseInt(document.getElementById('arrow').style.marginLeft.replace('px', ''), 10);
+
+            var direction = true;
+            var index;
+
+            if (posDKleft > marginLeftFond && clientWidthFond + marginLeftFond >= posDKleft) {
+                for (let i = 0; i <= 42; i++) {
+
+                    if (document.getElementById('mur' + i + '').style.marginLeft == posDKleft - 50 + 'px' && document.getElementById('mur' + i + '').style.marginTop == posDKtop + 'px') {
+                        index = i;
+                        direction = false
+
+                        //querySelectorAll
+
+                        break;
+                    }
+                }
+                if (direction == true) {
+                    //deplacement
+                    posDKleft = posDKleft - 50;
+
+                    //afficher
+                    document.getElementById('dk').src = "./img/dk_gauche.png"
+                    document.getElementById('dk').style.marginLeft = posDKleft + "px";
+                    document.getElementById('dk').style.marginTop = posDKtop + "px";
+
+                    Dead(posDKleft, posARROWleft, posDKtop, posARROWtop)
+                    //hidden
+                    //document.getElementById('dk').style.marginLeft = posDKleft + "px";
+                }
+            }
+        }
+
+        else if (e.keyCode == '39') {
+            
+            // right arrow(clavier)
+            let posDKtop = parseInt(document.getElementById('dk').style.marginTop.replace('px', ''), 10);
+            let posDKleft = parseInt(document.getElementById('dk').style.marginLeft.replace('px', ''), 10);
+            let widthDK = parseInt(document.getElementById('dk').style.width.replace('px', ''), 10);
+            let marginLeftFond = parseInt(document.getElementById('fond').style.marginLeft.replace('px', ''), 10);
+            let clientWidthFond = parseInt(document.getElementById('fond').clientWidth, 10);
+            var posARROWtop = parseInt(250);
+            var posARROWleft = parseInt(document.getElementById('arrow').style.marginLeft.replace('px', ''), 10);
+
+            var direction = true;
+            var index;
+
+            //Test du mur
+            if (posDKleft >= marginLeftFond && clientWidthFond + marginLeftFond - widthDK > posDKleft) {
+                for (let i = 0; i <= 42; i++) {
+
+
+                    if (document.getElementById('mur' + i + '').style.marginLeft == posDKleft + 50 + 'px' && document.getElementById('mur' + i + '').style.marginTop == posDKtop + 'px') {
+                        index = i;
+                        direction = false
+
+                        //querySelectorAll
+
+                        break;
+                    }
+                }
+
+                if (direction == true) {
+                    //deplacement
+                    posDKleft = posDKleft + 50;
+
+                    //afficher
+                    document.getElementById('dk').src = "./img/dk_droite.png"
+                    document.getElementById('dk').style.marginLeft = posDKleft + "px";
+                    document.getElementById('dk').style.marginTop = posDKtop + "px";
+
+                    //hidden
+                    //document.getElementById('dk').style.marginLeft = posDKleft + "px";
+
+                    //Animation fin
+                    if (document.getElementById('fin').style.marginLeft == posDKleft + 'px' && document.getElementById('fin').style.marginTop == posDKtop + 'px') {
+                        let fin;
+                        let sonFinDK = sonFin.innerHTML;
+                        
+                        document.getElementById('finAffichage').style.display = "";
+                        document.getElementById('finAffichage').style.zIndex = 4;
+
+                        Dead(posDKleft, posARROWleft, posDKtop, posARROWtop)
+                        fin = document.getElementsByClassName('stage1')[0];
+                        //fin.remove();
+
+                        //Audio fin
+                        sonFinDK = `<audio src="./img/Final.wav" id="sonFin" style="display:none" controls autoplay ></audio>`;
+                        sonFin.innerHTML = sonFinDK;
+                        document.getElementById('sonFin').play();
+                        setTimeout(document.getElementById('sonFin').pause(), 4000);
+
+                    }
+                }
+            }
+        }
+
+        else if (e.keyCode == '112') {
+            document.getElementById('fondStart').style.zIndex = 0;
+        }
+
+        else if (e.keyCode == '13') {
+            location.href = "index.html"
+        }
+    })
+
+}; 
+
+function deplacementArrow() {
+   var posDKtop = parseInt(document.getElementById('dk').style.marginTop.replace('px', ''), 10);
+   var posDKleft = parseInt(document.getElementById('dk').style.marginLeft.replace('px', ''), 10);
+   var posARROWtop = parseInt(250);
+   var posARROWleft = parseInt(document.getElementById('arrow').style.marginLeft.replace('px', ''), 10);
+   var marginLeftFond = 475;
+   var afin = false;
+   
+   //for(var p = 775; p>475; p=p-50){
+    while (posARROWleft > 525){
+    
+         document.getElementById("arrow").style.zIndex = 4;
+
+         posARROWleft = posARROWleft - 50;
+         document.getElementById("arrow").style.marginLeft = posARROWleft + "px";
+         return new Promise((resolve) => setTimeout(resolve, 1000)); // attendre 2 secondes avant de continuer
     }
-})
+ 
+   document.getElementById('arrow').style.zIndex = 0;
+   document.getElementById('arrow').style.marginLeft = '775px';
+
+}
+
+function Dead(posDKleft, posARROWleft, posDKtop, posARROWtop){
+    if (posDKleft == posARROWleft && posDKtop == posARROWtop) {
+        location.reload(); 
+      }   
+};
